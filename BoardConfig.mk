ad6dba6ae4e7f3688c549f8b797664bfd50db598
Sarthak Roy <sarthakroy2002@gmail.com>
2021-12-29 13:12:15 +0530

    fixup! RMX2020: Fix flashlight path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,5 +177,5 @@ TARGET_USES_LOGD := true
 PB_DISABLE_DEFAULT_TREBLE_COMP := true
 
 #PB Torch
-PB_TORCH_PATH := /proc/qcom_flash
+PB_TORCH_PATH := "/proc/qcom_flash"
 PB_TORCH_MAX_BRIGHTNESS := 1
